3a15ef256aa79bdfc1d26617608a04bf04340152
MakeMD <admin.rio@gmail.com>
2015-02-26 16:21:50 +0200

    add platimun
    add quantity drop for blocks

--- src/NewMetals.java
+++ src/NewMetals.java
@@ -1,5 +1,8 @@
 package assets.newmetals.src;
 
+import assets.newmetals.src.blocks.PlatinumBlock;
+import assets.newmetals.src.blocks.PlatinumIngotItem;
+import assets.newmetals.src.blocks.PlatinumOreBlock;
 import assets.newmetals.src.blocks.TitanBlock;
 import assets.newmetals.src.blocks.TitanIngotItem;
 import assets.newmetals.src.blocks.TitanOreBlock;
@@ -20,8 +23,8 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 @Mod(modid="newmetals", name=ModInfo.NAME, version=ModInfo.VERSION)
 
 public class NewMetals {
- public static Block zincoreblock, titanoreblock, titanblock;
- public static Item zincingotitem, titaningotitem;
+ public static Block zincoreblock, titanoreblock, titanblock, platinumoreblock, platinumblock;
+ public static Item zincingotitem, titaningotitem, platinumingotitem;
 	
        @EventHandler
     public void Load(FMLInitializationEvent event)
@@ -31,21 +34,33 @@ public class NewMetals {
     	   titanoreblock = new TitanOreBlock(1002).setUnlocalizedName("titanoreblock");
     	   titaningotitem = new TitanIngotItem(1003).setUnlocalizedName("titaningotitem");
     	   titanblock = new TitanBlock(1004).setUnlocalizedName("titanblock");
+    	   platinumoreblock = new PlatinumOreBlock(1005).setUnlocalizedName("platinumblock");
+    	   platinumblock = new PlatinumBlock(1006).setUnlocalizedName("platinumblock");
+    	   platinumingotitem = new PlatinumIngotItem(1007).setUnlocalizedName("platinumingotitem");
+    	   GameRegistry.registerBlock(platinumoreblock, "platinumoreblock");
+    	   GameRegistry.registerBlock(platinumblock, "platinumblock");
+    	   GameRegistry.registerItem(platinumingotitem, "paltinumingotitem");
     	   GameRegistry.registerBlock(zincoreblock, "zincoreblock");
     	   GameRegistry.registerItem(zincingotitem, "ziningotitem");
     	   GameRegistry.registerBlock(titanoreblock, "titanoreblock");
     	   GameRegistry.registerBlock(titanblock, "titanblock");
     	   GameRegistry.registerItem(titaningotitem, "titaningotitem");
+    	   
            LanguageRegistry.instance().addNameForObject(zincoreblock, "en_US", "Zinc Ore");
            LanguageRegistry.instance().addNameForObject(zincingotitem, "en_US", "Zinc Ingot");
            LanguageRegistry.instance().addNameForObject(titanoreblock, "en_US", "Titan Ore");
            LanguageRegistry.instance().addNameForObject(titaningotitem, "en_US", "Titan Ingot");
            LanguageRegistry.instance().addNameForObject(titanblock, "en_US", "Titan Block");
+           LanguageRegistry.instance().addNameForObject(platinumoreblock, "en_US", "Platinum Ore");
+           LanguageRegistry.instance().addNameForObject(platinumblock, "en_US", "Platinum Block");
+           LanguageRegistry.instance().addNameForObject(platinumingotitem, "en_US", "Platinum Ingot");
            
            GameRegistry.addShapelessRecipe(new ItemStack(Item.ingotGold, 4), new Object[] {Item.ingotGold, NewMetals.zincingotitem, NewMetals.zincingotitem});
            GameRegistry.addSmelting(NewMetals.zincoreblock.blockID, new ItemStack(NewMetals.zincingotitem, 1), 1.0F);
            GameRegistry.addRecipe(new ItemStack(NewMetals.titanblock, 1), new Object[]{"###", "###", "###", Character.valueOf('#'), NewMetals.titaningotitem});
-          GameRegistry.addSmelting(NewMetals.titanoreblock.blockID, new ItemStack(NewMetals.titaningotitem, 1), 1.0F);
+           GameRegistry.addSmelting(NewMetals.titanoreblock.blockID, new ItemStack(NewMetals.titaningotitem, 1), 1.0F);
+           GameRegistry.addSmelting(NewMetals.platinumoreblock.blockID, new ItemStack(NewMetals.platinumingotitem, 1), 1.0F);
+           GameRegistry.addRecipe(new ItemStack(NewMetals.platinumblock, 1), new Object[]{"###","###","###", Character.valueOf('#'), NewMetals.platinumingotitem});
     }
  
 }
--- src/blocks/PlatinumOreBlock.java
+++ src/blocks/PlatinumOreBlock.java
@@ -2,19 +2,19 @@ package assets.newmetals.src.blocks;
 
 import java.util.Random;
 
+import assets.newmetals.src.NewMetals;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import assets.newmetals.src.NewMetals;
 
-public class TitanBlock extends Block
+public class PlatinumOreBlock extends Block
 {
-	public TitanBlock(int par1)
+	public PlatinumOreBlock(int par1)
 	{
 		super(par1, Material.iron);
 		this.setCreativeTab(CreativeTabs.tabBlock);
-		this.setHardness(100F);
+		this.setHardness(1.0F);
 		this.setStepSound(soundMetalFootstep);
 	}
 
@@ -22,17 +22,12 @@ public class TitanBlock extends Block
 	public void registerIcons(IconRegister ir)
 	{
 		
-		this.blockIcon = ir.registerIcon("newmetals:TitanBlock");
+		this.blockIcon = ir.registerIcon("newmetals:PlatinumOreBlock");
 	}
-	@Override
-    public int idDropped(int par1, Random par2Random, int par3)
-    {
-     return NewMetals.titanblock.blockID;   
-	 //return Item.diamond.itemID;
-    }
-	public int quantityDropped(Random par1Random)
-	{
-		return 9;
-		
-	}
+	 @Override
+	    public int idDropped(int par1, Random par2Random, int par3)
+	    {
+	     return NewMetals.platinumingotitem.itemID;   
+		 //return Item.diamond.itemID;
+	    }
 }
